65796e9586b0defac922ae653e13ee08ece5d7f3
Peter Tamas <peter.tamas@greenfoxacademy.com>
2023-06-01 12:54:38 +0200

    HTML canvas, kepek, kep megjelenitese a canvason

--- minesweeper.js
+++ minesweeper.js
@@ -1,1 +1,5 @@
-console.log("Hello, világ!");
+var image = document.getElementById('hidden');
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+ctx.drawImage(image, 0, 0); // A kép bal felső sarka (0,0) koordinátára kerülß
+
