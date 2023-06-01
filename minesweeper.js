cf58653bd4916870f4ddebb28ae53a8262db1cfd
Peter Tamas <peter.tamas@greenfoxacademy.com>
2023-06-01 16:55:19 +0200

    for ciklus

--- minesweeper.js
+++ minesweeper.js
@@ -2,12 +2,13 @@ const image = document.getElementById('hidden');
 const canvas = document.getElementById('myCanvas');
 const c = canvas.getContext('2d');
 
-const size = 50;
+const imgSize = 50;
+const columns = canvas.width / imgSize;
 
-drawImage(0, 0);
-drawImage(100, 100);
-drawImage(123, 234);
+for (let i = 0; i < columns; i++) {
+  drawImage(i * imgSize, 0);
+}
 
 function drawImage(x, y) {
-  c.drawImage(image, x, y, size, size);
+  c.drawImage(image, x, y, imgSize, imgSize);
 }
