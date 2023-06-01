ecef42c4e58804a620261a3f742d1f296839fb0e
Peter Tamas <peter.tamas@greenfoxacademy.com>
2023-06-01 16:25:36 +0200

    drawImage fuggveny

--- minesweeper.js
+++ minesweeper.js
@@ -3,8 +3,11 @@ const canvas = document.getElementById('myCanvas');
 const c = canvas.getContext('2d');
 
 const size = 50;
-let x = 100;
-let y = 50;
 
-c.drawImage(image, x, y, size, size); // A kép bal felső sarka (0,0) koordinátára kerül
-c.drawImage(image, x + size, y + size, size, size);
+drawImage(0, 0);
+drawImage(100, 100);
+drawImage(123, 234);
+
+function drawImage(x, y) {
+  c.drawImage(image, x, y, size, size);
+}
