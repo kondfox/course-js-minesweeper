fef6c0a9cdff90a4bbc4fae8aed9a0dc7dd36523
Peter Tamas <peter.tamas@greenfoxacademy.com>
2023-06-01 16:07:30 +0200

    pozicionalas a canvason, valtozok

--- minesweeper.js
+++ minesweeper.js
@@ -1,5 +1,10 @@
-var image = document.getElementById('hidden');
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
-ctx.drawImage(image, 0, 0); // A kép bal felső sarka (0,0) koordinátára kerülß
+const image = document.getElementById('hidden');
+const canvas = document.getElementById('myCanvas');
+const c = canvas.getContext('2d');
 
+const size = 50;
+let x = 100;
+let y = 50;
+
+c.drawImage(image, x, y, size, size); // A kép bal felső sarka (0,0) koordinátára kerül
+c.drawImage(image, x + size, y + size, size, size);
